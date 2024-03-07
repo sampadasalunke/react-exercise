3b147fb56a4ad87af7ae8b45420eb2280e29525e
sampadasalunke <sampadasalunke0214@gmail.com>
2024-03-07 21:06:59 +0530

    Test data

---
+++ react-exercise/src/simple-data.js
@@ -0,0 +1,159 @@
+const data = [
+  {
+    id: 1,
+    name: "Enhancing ML Insights Functionality",
+    title: "S",
+    percentage: "0%",
+    managerId: null,
+    expanded: true,
+    employees: [
+      {
+        id: 2,
+        name: "Enhance the product's machine learning insights for better user decision-making",
+        title: "S",
+        percentage: "0%",
+        managerId: 1,
+        employees:[
+          { 
+            id: 3,
+            name: "Develop and launch 5 new ML-driven insights features",
+            title: "S",
+            percentage: "0%",
+            managerId: 2,
+          },
+          { 
+            id: 4,
+            name: "Achieve a 25% increase in user engagement with MLInsights features",
+            title: "S",
+            percentage: "0%",
+            managerId: 2,
+          },
+          {
+            id: 5,
+            name: "Receive positive feedback (above 4/5) from 70% of surveyed users on the new features",
+            title: "S",
+            percentage: "0%",
+            managerId: 2,
+          }
+        ]
+      },
+      {
+        id: 6,
+        name: "Use AI-driven analytics to identify areas for product improvement",
+        title: "S",
+        percentage: "0%",
+        managerId: 1,
+        employees:[
+          {
+            id: 7,
+            name: "Identify and implement 5 major product improvements based on AI analytics",
+            title: "S",
+            percentage: "0%",
+            managerId: 6,
+          },
+          {
+            id: 8,
+            name: "Increase product functionality satisfaction by 20% as per user surveys",
+            title: "S",
+            percentage: "0%",
+            managerId: 6,
+          },
+          {
+            id: 9,
+            name: "Reduce feature-related user recommendations by 30%",
+            title: "S",
+            percentage: "0%",
+            managerId: 6,
+          }
+        ]
+      },
+      {
+        id: 10,
+        name: "Train the analytics system with historical data to establish a baseline for comparison and improvement",
+        title: "S",
+        percentage: "0%",
+        managerId: 1,
+        employees:[
+          {
+            id: 11,
+            name: "Collect and analyze user interaction data with the product using the AI analytics system",
+            title: "S",
+            percentage: "0%",
+            managerId: 10,
+          }
+        ]
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: "Generative AI Integration",
+    title: "S",
+    percentage: "0%",
+    managerId: null,
+    expanded: true,
+    employees: [
+      {
+        id: 13,
+        name: "Successfully integrate Generative AI capabilities into the existing product",
+        title: "S",
+        percentage: "0%",
+        managerId: 12,
+        employees:[
+          { 
+            id: 14,
+            name: "Complete integration of GenAl features by Q2",
+            title: "S",
+            percentage: "0%",
+            managerId: 13,
+          },
+          { 
+            id: 15,
+            name: "Achieve a user adoption rate of 20% for the GenAl feature within the first three months",
+            title: "S",
+            percentage: "0%",
+            managerId: 13,
+          },
+          {
+            id: 16,
+            name: "Receive a user satisfaction rating of 4/5 for the new GenAl feature",
+            title: "S",
+            percentage: "0%",
+            managerId: 13,
+          }
+        ]
+      },
+      {
+        id: 17,
+        name: "Utilize GenAl to create high-quality, engaging content for the platform",
+        title: "S",
+        percentage: "0%",
+        managerId: 12,
+        employees:[
+          {
+            id: 18,
+            name: "Produce 15% of the content using GenAl tools",
+            title: "S",
+            percentage: "0%",
+            managerId: 17,
+          },
+          {
+            id: 19,
+            name: "Achieve a user engagement rate that matches or exceeds manually created content",
+            title: "S",
+            percentage: "0%",
+            managerId: 17,
+          },
+          {
+            id: 20,
+            name: "Receive positive user feedback (4/5 or higher) for GenAl-created content",
+            title: "S",
+            percentage: "0%",
+            managerId: 17,
+          },
+        ]
+      },
+    ],
+  },
+];
+export default data;
